b38fe2e7859626185e1c7fab6f985c36cc47a9ee
Zanhua1999 <skyair886@gmail.com>
2020-02-07 15:21:19 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,8 @@
+
+ var h1 = document.querySelector("h1");
+ h1.classList.add("tekst");
+
+ var sap = document.querySelectorAll("p");
+ sap.forEach(function(element){
+    element.classList.add('sample');
+ });
